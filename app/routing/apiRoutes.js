916e31f0f29a0c32f1c34b5ff1b0dac2e83caebf
rperez2021 <rob.perez.flores@gmail.com>
2018-04-24 08:52:00 -0700

    Updated routing

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+
+//Firebase DB Setup
+var firebase = require('firebase');
+var config = {
+    apiKey: "AIzaSyB35Azq0vglMeEo4yo6NHjNwRkDIvzRWxo",
+    authDomain: "pet-pal-finder.firebaseapp.com",
+    databaseURL: "https://pet-pal-finder.firebaseio.com",
+    projectId: "pet-pal-finder",
+    storageBucket: "pet-pal-finder.appspot.com",
+    messagingSenderId: "318967169872"
+};
+firebase.initializeApp(config);
+
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//This will be used to display a JSON of all possible friends.
+app.get('/api/friends', (req, res) => {
+    res.sendFile(path.join(__dirname, "../data/petpals.json"));
+})
+
+app.use("/api/friends", (req, res) => {
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('you posted:\n')
+    console.log(req.body)
+    res.end(JSON.stringify(req.body, null, 2))
+    
+})
+
+app.use(function (req, res, next) {
+    res.status(404).sendFile(path.join(__dirname, "../public/home.html"))
+})
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('500! Something broke! And the Error Handler was used!')
+})
+
+
+
+app.listen(3000, () => console.log('Petpal is Listening on localhost:3000'))
+
+function totalDifference() {
+
+}
+
+
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,22 +3,50 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 
-var homepage = path.basename('...\app\public\survey.html');
-// Returns: 'myfile.html'
+//Firebase DB Setup
+var firebase = require('firebase');
+var config = {
+    apiKey: "AIzaSyB35Azq0vglMeEo4yo6NHjNwRkDIvzRWxo",
+    authDomain: "pet-pal-finder.firebaseapp.com",
+    databaseURL: "https://pet-pal-finder.firebaseio.com",
+    projectId: "pet-pal-finder",
+    storageBucket: "pet-pal-finder.appspot.com",
+    messagingSenderId: "318967169872"
+};
+firebase.initializeApp(config);
 
 //Body Parser Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//Set Static Path
-app.use(express.static(path.join(__dirname, 'public')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+})
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/survey', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+})
 
-app.listen(3000, () => console.log('Listening on localhost:3000'))
+//This will be used to display a JSON of all possible friends.
+app.get('/api/friends', (req, res) => {
+    res.sendFile(path.join(__dirname, "../data/petpals.json"));
+})
+
+app.use("/api/friends", (req, res) => {
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('you posted:\n')
+    var new_pet_score = req.body.scores
+    new_pet_score.forEach(element => {
+        
+    });
+    console.log(req.body)
+    console.log(req.body.scores)
+    res.end(JSON.stringify(req.body, null, 2))
+    
+})
 
 app.use(function (req, res, next) {
-    res.status(404).send("404! Sorry can't find that!")
+    res.status(404).sendFile(path.join(__dirname, "../public/home.html"))
 })
 
 app.use(function (err, req, res, next) {
@@ -26,6 +54,9 @@ app.use(function (err, req, res, next) {
     res.status(500).send('500! Something broke! And the Error Handler was used!')
 })
 
+
+
+app.listen(3000, () => console.log('Petpal is Listening on localhost:3000'))
 
 
 
