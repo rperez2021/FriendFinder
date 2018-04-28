b73465dc08bf3462533636c897f16595216ba5a3
rperez2021 <rob.perez.flores@gmail.com>
2018-04-28 00:27:56 -0700

    Project functional

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,14 +2,14 @@ var firebase = require('firebase');
 var fs = require('fs');
 
 
-module.exports = function(app) {
-var pals = firebase.database().ref();
-pals.on('value', function(snapshot) {
-    fs.writeFile('./app/data/petpals.js', 'var array = ['+JSON.stringify(snapshot.val())+']', 'utf8', (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      });
-    
-});
+module.exports = function (app) {
+    var pals = firebase.database().ref();
+    pals.on('value', function (snapshot) {
+        fs.writeFile('./app/data/petpals.json', JSON.stringify(snapshot.val()), 'utf8', (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
+
+    });
 
 }
--- app/data/petpals.js
+++ app/data/petpals.js
@@ -1,1 +1,68 @@
-var array = [{ "abb": { "name": "abb", "photo": "asdasdad", "scores": ["2", "5", "3", "2", "3", "5", "3", "2", "5", "2"] }, "asdasd": { "name": "asdasd", "photo": "asdasdad", "scores": ["2", "5", "3", "2", "3", "5", "3", "2", "5", "2"] } }]
+var palsArray = [
+    {
+        name: "Archer",
+        photo: "https://brevity-is.com/img/team/3a.jpg",
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        name: "Max",
+        photo: "http://cdn2-www.dogtime.com/assets/uploads/gallery/golden-retriever-dogs-and-puppies/golden-retriever-dogs-puppies-10.jpg",
+        scores: [
+            "2",
+            "5",
+            "3",
+            "2",
+            "3",
+            "5",
+            "3",
+            "2",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Nella",
+        photo: "https://vetstreet.brightspotcdn.com/dims4/default/94ad13b/2147483647/thumbnail/645x380/quality/90/?url=https%3A%2F%2Fvetstreet-brightspot.s3.amazonaws.com%2F52%2F245d60a8a511e0a0d50050568d634f%2Ffile%2Fpoodle-1-645mk070411.jpg",
+        scores: [
+            "2",
+            "5",
+            "3",
+            "2",
+            "3",
+            "5",
+            "3",
+            "2",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: "Lily",
+        photo: "http://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12233900/Doberman-Pinscher-On-White-01.jpg",
+        scores: [
+            "2",
+            "5",
+            "3",
+            "2",
+            "3",
+            "5",
+            "3",
+            "2",
+            "5",
+            "2"
+        ]
+    }
+];
+
+module.exports = palsArray;
