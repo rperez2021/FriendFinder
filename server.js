33b59ce1f68678b1e65a0d3c4e9dd1da63f8f79c
rperez2021 <rob.perez.flores@gmail.com>
2018-04-28 00:38:23 -0700

    Updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var app = express();
+var PORT = process.env.PORT;
 var firebase = require('firebase');
 
 var config = {
@@ -22,4 +23,4 @@ require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 require(path.join(__dirname, './app/data/friends'))(app);
 
-app.listen(3000, () => console.log('Petpal is Listening on localhost:3000'))
+app.listen(PORT, () => console.log('Petpal is Listening on localhost:' + PORT))
